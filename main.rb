f8632025d8747f70b5e2bcd2fa677c53d266275d
Bradley Buda <bradleybuda@gmail.com>
2016-04-27 19:41:32 -0700

    Add some really basic AI

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+require 'logger'
+
+$logger = Logger.new(STDOUT)
+$logger.level = Logger::INFO
+
+
 TEAMS = 4
 INITIAL_ROSTER = [4,3]
 DRAFT_ROUNDS = 8
@@ -16,21 +22,81 @@ PLAYERS = [10,
 
 raise unless PLAYERS.size == TEAMS * (INITIAL_ROSTER.size + DRAFT_ROUNDS)
 
-INITIAL_DRAFT_ORDER = [
-  [0,1,2,3, 3,2,1,0, 3,1,0,2, 2,0,1,3, 2,1,0,3, 3,0,1,2, 2,1,0,3, 3,0,1,2],
+CHAMPIONSHIPS_TO_WIN = 3
+
+# TODO pick a sequence we like and stick with it
+PERMUTATIONS = [
+  [0, 1, 2, 3],
+  [1, 3, 0, 2],
+  [2, 1, 3, 0],
+  [3, 2, 0, 1],
+  [0, 3, 1, 2],
+  [1, 0, 2, 3],
+  [2, 3, 1, 0],
+  [3, 0, 2, 1],
+  [0, 2, 1, 3],
 ]
 
-raise unless INITIAL_DRAFT_ORDER[0].size == (DRAFT_ROUNDS * TEAMS)
+raise unless PERMUTATIONS.size == ((CHAMPIONSHIPS_TO_WIN - 1) * TEAMS) + 1
+
+def permute_array(array, permutation_index)
+  mapping = PERMUTATIONS[permutation_index]
+  array.map { |i| mapping[i] }
+end
+
+GOFIRST_DRAFT_ORDER =  [0,1,2,3, 3,2,1,0, 3,1,0,2, 2,0,1,3, 2,1,0,3, 3,0,1,2, 2,1,0,3, 3,0,1,2]
+raise unless GOFIRST_DRAFT_ORDER.size == (DRAFT_ROUNDS * TEAMS)
+
+#puts draft_order.map{ |d| d.join(",") }.join("\n")
+
+PERSONALITIES = {
+  "Warriors" => {pass_rates: {final: 0.1, semifinal: 0.3}, greedy: true, dont_waste_cards: true},
+  "Spurs" => {pass_rates: {final: 0.2, semifinal: 0.5}, greedy: true, dont_waste_cards: true},
+  "Cavs" => {pass_rates: {final: 0.2, semifinal: 0.3}, greedy: true, dont_waste_cards: true},
+  "Celtics" => {pass_rates: {final: 0.1, semifinal: 0.6}, greedy: true, dont_waste_cards: true},
+}
+
+
+# TODO strategy!
+# don't play all of your cards in a semifinal (unless the other guy already did and you're the home team)
+class AiAgent
+  def initialize(team, personality)
+    @team = team
+    @personality = personality
+  end
+
+  def make_move_by_pick
+    if @personality[:greedy]
+      @team.roster.sort.last
+    else
+      @team.roster.shuffle.first
+    end
+  end
+
+  def make_move(stage)
+    if (@personality[:dont_waste_cards] && (stage == :final) && (@team.roster.size > 2))
+      # No point in passing
+      make_move_by_pick
+    elsif Random.rand < @personality[:pass_rates][stage]
+      :pass
+    else
+      make_move_by_pick
+    end
+  end
+end
+
 
 class Team
   attr_reader :roster
   attr_accessor :championships
   attr_reader :name
+  attr_reader :agent
 
   def initialize(idx)
     @name = ["Warriors", "Spurs", "Cavs", "Celtics"][idx]
     @roster = []
     @championships = 0
+    @agent = AiAgent.new(self, PERSONALITIES[@name])
   end
 
   def strength
@@ -42,10 +108,11 @@ class MatchParticipant
   attr_reader :team
   attr_reader :cards_played
 
-  def initialize(team)
+  def initialize(team, stage)
     @team = team
     @cards_played = []
     @passed = false
+    @stage = stage
   end
 
   def passed?
@@ -60,30 +127,28 @@ class MatchParticipant
     end
   end
 
-  # TODO strategy!
-  # for one, don't pass with more than two cards in hand if you're in the final
-  # don't play all of your cards in a semifinal (unless the other guy already did and you're the home team)
   def make_move
     return if @passed
 
     if @team.roster.empty?
       # if out of cards, have to pass
       @passed = true
-    elsif Random.rand < 0.3
-      # Randomly pass
-      @passed = true
     else
-      # Play a random card
-      @team.roster.shuffle!
-      @cards_played << @team.roster.shift
+      play = @team.agent.make_move(@stage)
+      if play == :pass
+        @passed = true
+      else
+        @cards_played << play
+        @team.roster.delete_at(@team.roster.find_index(play))
+      end
     end
   end
 end
 
 class Match
-  def initialize(home_team, away_team)
-    @home = MatchParticipant.new(home_team)
-    @away = MatchParticipant.new(away_team)
+  def initialize(home_team, away_team, stage)
+    @home = MatchParticipant.new(home_team, stage)
+    @away = MatchParticipant.new(away_team, stage)
   end
 
   def play!
@@ -108,74 +173,89 @@ class Match
   end
 end
 
-teams = Array.new(TEAMS) { |i| Team.new(i) }
-free_agents = PLAYERS.dup
-
-# Pre-game - give each team their initial roster
-
-teams.each do |team|
-  INITIAL_ROSTER.each do |rank|
-    team.roster << rank
-    free_agents.delete_at(free_agents.find_index(rank))
-  end
-end
-
-season = 0
-
-while teams.all? { |t| t.championships < 3 }
-  puts
-  puts "Season #{season} starting!"
-
-  # loop through seasons until one team has three championships
-  # three phases to season: 1) draft (with trades), 2) tournament, 3) offseason (retain up to two)
-
-  # Phase 1: Draft
-
-  # Do a straight draft w/o trades
-  # TODO permute draft order by season
-  free_agents.sort!.reverse!
-  INITIAL_DRAFT_ORDER[0].each do |team_to_draft_next|
-    team = teams[team_to_draft_next]
-    next_free_agent = free_agents.shift
-    team.roster <<  next_free_agent
-    puts "#{team.name} pick #{next_free_agent}"
+def play_game!
+  draft_order = (0...PERMUTATIONS.size).map do |season|
+    permute_array(GOFIRST_DRAFT_ORDER, season)
   end
 
-  # Phase 2: Tournament
-  # TODO permute home/away and matchups by season
-  east_semi = Match.new(teams[0],teams[1])
-  east_winner = east_semi.play!
-  puts "East semifinal:"
-  puts east_semi.summary
+  teams = Array.new(TEAMS) { |i| Team.new(i) }
+  teams.shuffle!
+  free_agents = PLAYERS.dup
 
-  west_semi = Match.new(teams[2],teams[3])
-  west_winner = west_semi.play!
-  puts "West semifinal:"
-  puts west_semi.summary
+  # Pre-game - give each team their initial roster
 
-  final = Match.new(east_winner, west_winner)
-  champion = final.play!
-  puts "Final:"
-  puts final.summary
-  champion.championships += 1
-
-  # Return played cards to free agency
-  [east_semi, west_semi, final].each { |match| free_agents.push(*match.cards_played) }
-
-
-  # Phase 3: Offseason
-
-  # Draw down each team to best two keepers
-  # TODO is there ever a reason for a team to voluntarily keep less than two? I don't think so...
   teams.each do |team|
-    team.roster.sort!
-    while team.roster.size > 2
-      free_agents << team.roster.shift
+    INITIAL_ROSTER.each do |rank|
+      team.roster << rank
+      free_agents.delete_at(free_agents.find_index(rank))
     end
   end
 
-  puts "Keepers:"
-  p teams
+  season = 0
 
-  season += 1
+  while teams.all? { |t| t.championships < 3 }
+    $logger.debug "Season #{season} starting!"
+
+    # loop through seasons until one team has three championships
+    # three phases to season: 1) draft (with trades), 2) tournament, 3) offseason (retain up to two)
+
+    # Phase 1: Draft
+
+    # Do a straight draft w/o trades
+    # TODO permute draft order by season
+    free_agents.sort!.reverse!
+    draft_order[season].each do |team_to_draft_next|
+      team = teams[team_to_draft_next]
+      next_free_agent = free_agents.shift
+      team.roster <<  next_free_agent
+      $logger.debug "#{team.name} pick #{next_free_agent}"
+    end
+
+    # Phase 2: Tournament
+    play_order = permute_array([0,1,2,3], season)
+    east_semi = Match.new(teams[play_order[0]],teams[play_order[1]],:semifinal)
+    east_winner = east_semi.play!
+    $logger.debug "East semifinal: #{east_semi.summary}"
+
+    west_semi = Match.new(teams[play_order[2]],teams[play_order[3]],:semifinal)
+    west_winner = west_semi.play!
+    $logger.debug "West semifinal: #{west_semi.summary}"
+
+    final = Match.new(east_winner, west_winner,:final)
+    champion = final.play!
+    $logger.debug "Final: #{final.summary}"
+    champion.championships += 1
+
+    # Return played cards to free agency
+    [east_semi, west_semi, final].each { |match| free_agents.push(*match.cards_played) }
+
+
+    # Phase 3: Offseason
+
+    # Draw down each team to best two keepers
+    # TODO is there ever a reason for a team to voluntarily keep less than two? I don't think so...
+    teams.each do |team|
+      team.roster.sort!
+      while team.roster.size > 2
+        free_agents << team.roster.shift
+      end
+    end
+
+    $logger.debug "Keepers: #{teams.inspect}"
+
+    season += 1
+  end
+
+  teams.find { |t| t.championships == 3 }
 end
+
+win_rate = {}
+
+10000.times do
+  winner = play_game!
+  win_rate[winner.name] ||= 0
+  win_rate[winner.name] += 1
+end
+
+$logger.info PERSONALITIES.inspect
+$logger.info win_rate.inspect
