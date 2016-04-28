39b7194215480bbcd1bd688a15d0734e2eae3306
Bradley Buda <bradleybuda@gmail.com>
2016-04-28 12:51:52 -0700

    Give the AI some common sense, add an optional human player

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'logger'
 
 $logger = Logger.new(STDOUT)
-$logger.level = Logger::INFO
+$logger.level = Logger::DEBUG
 
 
 TEAMS = 4
@@ -49,6 +49,7 @@ raise unless GOFIRST_DRAFT_ORDER.size == (DRAFT_ROUNDS * TEAMS)
 
 #puts draft_order.map{ |d| d.join(",") }.join("\n")
 
+# TODO optimize AIs - search, GA, etc
 PERSONALITIES = {
   "Warriors" => {pass_rates: {final: 0.1, semifinal: 0.3}, greedy: true, dont_waste_cards: true},
   "Spurs" => {pass_rates: {final: 0.2, semifinal: 0.5}, greedy: true, dont_waste_cards: true},
@@ -56,15 +57,33 @@ PERSONALITIES = {
   "Celtics" => {pass_rates: {final: 0.1, semifinal: 0.6}, greedy: true, dont_waste_cards: true},
 }
 
+class HumanAgent
+  def initialize(team)
+    @team = team
+  end
 
-# TODO strategy!
-# don't play all of your cards in a semifinal (unless the other guy already did and you're the home team)
+  def make_move(match)
+    puts "You are the #{@team.name}"
+    puts "Your hand: #{@team.roster.sort}"
+    puts "Card to play, or blank to pass?"
+    play = gets
+    play.chomp!
+    if play.empty?
+      :pass
+    else
+      # TODO engine should validate play
+      play.to_i
+    end
+  end
+end
+
 class AiAgent
   def initialize(team, personality)
     @team = team
     @personality = personality
   end
 
+  # TODO aggression parameter - don't be strictly random or greedy, but progressively slow
   def make_move_by_pick
     if @personality[:greedy]
       @team.roster.sort.last
@@ -73,11 +92,52 @@ class AiAgent
     end
   end
 
-  def make_move(stage)
-    if (@personality[:dont_waste_cards] && (stage == :final) && (@team.roster.size > 2))
-      # No point in passing
+  def throw_off_or_pass
+    if @team.roster.size > 2
+      @team.roster.sort.first
+    else
+      :pass
+    end
+  end
+
+  def make_move(match)
+    # Strategic TODOs
+    # * Size opponent's play (min, max, mean) based on knowledge of their hand and # of cards played
+    # * Determine if it's impossible to win and give up (min exceeds your hand total)
+    # * Determine if you've already won and stop playing (max is below your played card)
+    # * Play until your total exceeds your best estimate of their total, and no more
+    # * don't play all of your cards in a semifinal (unless the other side of the bracket already did and you're the home team)
+    # * don't pass if you're the opener and you have more than two cards
+    # * pass rate paramaterized on cards played / remaining
+
+    # Calculate some hand metrics
+    opponent_starting_hand = match.opponent_starting_hand(@team)
+    opponent_card_count = match.opponent_card_count(@team)
+
+    metrics = {
+      worst_current_opponent_score: opponent_starting_hand.sort.first(opponent_card_count).reduce(&:+) || 0,
+      best_current_opponent_score: opponent_starting_hand.sort.last(opponent_card_count).reduce(&:+) || 0,
+      best_possible_opponent_score: opponent_starting_hand.reduce(&:+) || 0,
+      my_score: match.my_score(@team),
+      my_best_possible_score: match.starting_hand(@team).reduce(&:+) || 0,
+    }
+
+    if match.opponent_has_passed(@team)
+      metrics[:best_possible_opponent_score] = metrics[:best_current_opponent_score]
+    end
+
+    $logger.debug "Hand analysis: #{metrics.inspect}"
+
+    if metrics[:my_score] > metrics[:best_possible_opponent_score]
+      $logger.debug "Victory is guaranteed, passing"
+      :pass
+    elsif metrics[:my_best_possible_score] < metrics[:worst_current_opponent_score]
+      $logger.debug "No win situation, throwing off"
+      throw_off_or_pass
+    elsif (@personality[:dont_waste_cards] && (match.stage == :final) && (@team.roster.size > 2))
+      # TODO No point in passing, *but* I should be playing my worst cards in this mode
       make_move_by_pick
-    elsif Random.rand < @personality[:pass_rates][stage]
+    elsif Random.rand < @personality[:pass_rates][match.stage]
       :pass
     else
       make_move_by_pick
@@ -92,11 +152,12 @@ class Team
   attr_reader :name
   attr_reader :agent
 
-  def initialize(idx)
+  def initialize(idx, human: false)
     @name = ["Warriors", "Spurs", "Cavs", "Celtics"][idx]
     @roster = []
     @championships = 0
-    @agent = AiAgent.new(self, PERSONALITIES[@name])
+
+    @agent = human ? HumanAgent.new(self) : AiAgent.new(self, PERSONALITIES[@name])
   end
 
   def strength
@@ -108,11 +169,11 @@ class MatchParticipant
   attr_reader :team
   attr_reader :cards_played
 
-  def initialize(team, stage)
+  def initialize(match, team)
+    @match = match
     @team = team
     @cards_played = []
     @passed = false
-    @stage = stage
   end
 
   def passed?
@@ -133,29 +194,74 @@ class MatchParticipant
     if @team.roster.empty?
       # if out of cards, have to pass
       @passed = true
+      $logger.debug "#{@team.name} are out of cards, passing"
     else
-      play = @team.agent.make_move(@stage)
+      play = @team.agent.make_move(@match)
       if play == :pass
         @passed = true
+        $logger.debug "#{@team.name} are electing to pass"
       else
-        @cards_played << play
-        @team.roster.delete_at(@team.roster.find_index(play))
+        if play_index = @team.roster.find_index(play)
+          @cards_played << play
+          @team.roster.delete_at(play_index)
+          $logger.debug "#{@team.name} played a card"
+        else
+          raise "illegal play: #{play}"
+        end
       end
     end
   end
 end
 
 class Match
+  attr_reader :stage
+
   def initialize(home_team, away_team, stage)
-    @home = MatchParticipant.new(home_team, stage)
-    @away = MatchParticipant.new(away_team, stage)
+    @home = MatchParticipant.new(self, home_team)
+    @away = MatchParticipant.new(self, away_team)
+    @stage = stage
+
+    @starting_hands = {
+      home_team => home_team.roster.dup,
+      away_team => away_team.roster.dup,
+    }
+  end
+
+  def my_score(team)
+    [@home, @away].find { |mp| mp.team == team }.cards_played.reduce(&:+) || 0
+  end
+
+  def starting_hand(team)
+    @starting_hands[team]
+  end
+
+  def opponent_has_passed(team)
+    [@home, @away].find { |mp| mp.team != team }.passed?
+  end
+
+  def opponent_starting_hand(team)
+    @starting_hands[opponent(team)]
+  end
+
+  def opponent_card_count(team)
+    [@home, @away].find { |mp| mp.team != team }.cards_played.count
+  end
+
+  def opponent(team)
+    [@home, @away].map(&:team).find { |t| t != team }
   end
 
   def play!
+    $logger.debug "#{@home.team.name} have #{@home.team.roster.sort}"
+    $logger.debug "#{@away.team.name} have #{@away.team.roster.sort}"
+
     until [@away,@home].all?(&:passed?)
       @away.make_move
       @home.make_move
     end
+
+    $logger.debug "#{@home.team.name} played #{@home.cards_played.sort} for #{@home.score}"
+    $logger.debug "#{@away.team.name} played #{@away.cards_played.sort} for #{@away.score}"
 
     if @away.score > @home.score
       return @away.team
@@ -179,6 +285,7 @@ def play_game!
   end
 
   teams = Array.new(TEAMS) { |i| Team.new(i) }
+  #teams[0] = Team.new(0, human: true)
   teams.shuffle!
   free_agents = PLAYERS.dup
 
@@ -251,7 +358,7 @@ end
 
 win_rate = {}
 
-10000.times do
+1.times do
   winner = play_game!
   win_rate[winner.name] ||= 0
   win_rate[winner.name] += 1
